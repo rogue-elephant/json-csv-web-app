4ab05f0ae6680a8bcfb23292503d83d7976839cf
DRandell <jup.trandell@gmail.com>
2019-10-26 18:38:52 +0100

    git ignore updates

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,4 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -9,13 +6,19 @@ var __importStar = (this && this.__importStar) || function (mod) {
     result["default"] = mod;
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const bodyParser = __importStar(require("body-parser"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const routes = __importStar(require("./routes"));
 dotenv_1.default.config();
 const app = express_1.default();
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(express_1.default.json());
 const port = process.env.SERVER_PORT; // default port to listen
 // using EJS for the view engine
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as bodyParser from "body-parser";
 import dotenv from "dotenv";
 import express from "express";
 import path from "path";
@@ -5,18 +6,20 @@ import * as routes from './routes'
 
 dotenv.config();
 const app = express();
-app.use( express.json() );
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(express.json());
 const port = process.env.SERVER_PORT; // default port to listen
 
 // using EJS for the view engine
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use( express.static( path.join( __dirname, "public" ) ) );
+app.use(express.static(path.join(__dirname, "public")));
 
 routes.registerRoutes(app);
 
 // start the Express server
-app.listen( port, () => {
+app.listen(port, () => {
     // console.log( `server started at http://localhost:${ port }` );
-} );
+});
