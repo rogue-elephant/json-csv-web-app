49443f639a1ca901403a664e69c41aa1ac0286d7
DRandell <jup.trandell@gmail.com>
2019-12-06 12:32:24 +0000

    black and white list now addable/removeable arrays and now have multiple tables with json-csv-tool update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,113 @@
-import { Component, OnInit } from '@angular/core';
-import {JsonCsvConverter, IJsonToCsvConversionStrategy, ConvertedCsv} from 'json-csv-tool';
-import { IRowValue } from 'json-csv-tool/lib/models/converted-csv';
-import { MatTableDataSource } from '@angular/material';
-import { saveAs } from 'file-saver';
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
+import {
+  JsonCsvConverter,
+  IJsonToCsvConversionStrategy,
+  Table
+} from "json-csv-tool";
+import { IRowValue } from "json-csv-tool/lib/models/converted-csv";
+import { MatTableDataSource } from "@angular/material";
+import { saveAs } from "file-saver";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'json-csv-web-app';
+export class AppComponent implements OnInit {
+  title = "json-csv-web-app";
   jsonInput: any;
   jsonModel: any;
-  convertedCsv: ConvertedCsv;
-  converterOptions: IJsonToCsvConversionStrategy = {};
+  convertedTable: Table;
+  matTables: {
+    title: string;
+    matTable: MatTableDataSource<any>;
+    convertedtTable: Table;
+  }[] = [];
+  converterOptions: IJsonToCsvConversionStrategy = { whiteList: [], blackList: [] };
+  currentWhitelist: string;
+  currentBlacklist: string;
+  loading = true;
 
   tableData: MatTableDataSource<any>;
 
-  updateJsonModel() {
-    try {
-      const rowData = [];
-      this.jsonModel = JSON.parse(this.jsonInput);
-      const converter = new JsonCsvConverter();
-      const convertedJson = converter.convertJsonToCsv(this.jsonModel, this.converterOptions);
-      this.convertedCsv = convertedJson;
-
-      this.convertedCsv.rows.forEach(row => {
-        const rowValue = {};
-        this.convertedCsv.columnNames.forEach(colName => {
-          rowValue[colName] = row.filter(x => x.columnName === colName)[0].value;
-        });
-        rowData.push(rowValue);
-      });
-      this.tableData = new MatTableDataSource(rowData);
-    } catch (error) {
-
-    }
+  ngOnInit() {
+    this.loading = false;
   }
 
-  getRowColValue = (row: IRowValue[], columnName: string) => row.filter(x => x.columnName === columnName)[0].value;
+  trackByIndex = (index: number, obj: any): any => index;
+
+  updateJsonModel() {
+    this.loading = true;
+    try {
+      this.jsonModel = JSON.parse(this.jsonInput);
+      const converter = new JsonCsvConverter();
+      const convertedJson = converter.convertJsonToCsv(
+        this.jsonModel,
+        {
+          ...this.converterOptions,
+          blackList: this.converterOptions.blackList.length > 0 ? this.converterOptions.blackList : null,
+          whiteList: this.converterOptions.whiteList.length > 0 ? this.converterOptions.whiteList : null
+        }
+      );
+      this.matTables = [];
+      this.convertedTable = convertedJson;
+      if(!this.convertedTable.title && this.convertedTable.rows.length > 0) {
+        this.convertedTable.title = this.convertedTable.rows[0][0].columnName;
+        this.convertedTable.title += this.convertedTable.title[this.convertedTable.title.length] === 's' ? 'es' : 's';
+      }
+
+      this.tableData = this.tableToMatTable(this.convertedTable);
+      this.matTables.splice(0, 0, {
+        title: this.convertedTable.title || "Table",
+        matTable: this.tableData,
+        convertedtTable: this.convertedTable
+      });
+    } catch (error) {}
+
+    this.loading = false;
+  }
+
+  tableToMatTable = (table: Table) => {
+    const rowData = [];
+    table.rows.forEach(row => {
+      const rowValue = {};
+      table.columnNames.forEach(colName => {
+        const rowCol = row.filter(x => x.columnName === colName)[0];
+        rowValue[colName] = rowCol.value;
+
+        if (rowCol.linkedTable) {
+          this.matTables.push({
+            title: `${row[0].value}: ${rowCol.linkedTable.title}`,
+            matTable: this.tableToMatTable(rowCol.linkedTable),
+            convertedtTable: rowCol.linkedTable
+          });
+        }
+      });
+      rowData.push(rowValue);
+    });
+    return new MatTableDataSource(rowData);
+  };
+
+  getRowColValue = (row: IRowValue[], columnName: string) =>
+    row.filter(x => x.columnName === columnName)[0].value;
 
   downloadFile() {
-    const blob = new Blob([this.convertedCsv.csv], {type: "text/plain;charset=utf-8"});
-    saveAs(blob, `${this.convertedCsv.title}.csv`);
+    const blob = new Blob([this.convertedTable.csv], {
+      type: "text/plain;charset=utf-8"
+    });
+    saveAs(blob, `${this.convertedTable.title}.csv`);
+  }
+
+  addToList(list, converterList) {
+    if (list) {
+      converterList.push(list);
+    }
+    this.currentWhitelist = this.currentBlacklist = '';
+    this.updateJsonModel();
+  }
+
+  removeFromList(index, converterList: any[]) {
+    converterList.splice(index, 1);
+    this.updateJsonModel();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatIconModule } from "@angular/material/icon";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatStepperModule } from "@angular/material/stepper";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material";
-import { MatButtonModule } from "@angular/material/button";
-import { MatTableModule } from "@angular/material/table";
-import { CdkColumnDef } from "@angular/cdk/table";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatGridListModule } from "@angular/material/grid-list";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { CdkColumnDef } from '@angular/cdk/table';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [AppComponent],
@@ -32,7 +34,9 @@ import { MatGridListModule } from "@angular/material/grid-list";
     MatButtonModule,
     MatTableModule,
     MatExpansionModule,
-    MatGridListModule
+    MatGridListModule,
+    MatProgressSpinnerModule,
+    MatListModule
   ],
   providers: [CdkColumnDef],
   bootstrap: [AppComponent]
